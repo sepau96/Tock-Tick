ac3521c8a82466ffb444197c687c37ff494b802a
sepau96 <48688723+sepau96@users.noreply.github.com>
2021-03-01 13:34:16 +0100

    vistas

--- src/server/config.js
+++ src/server/config.js
@@ -14,7 +14,7 @@ const errorHandler = require('errorHandler')
 module.exports = app => {
     app.set('port',process.env.PORT || 3000);
 
-    app.set('views', path.join(__dirname, 'views'))
+    app.set('views', path.join(__dirname, '../views'))
     app.engine('.hbs', exphbs({
         defaultLayout: 'main',
         partialsDir: path.join(app.get('views'), 'partials'),
